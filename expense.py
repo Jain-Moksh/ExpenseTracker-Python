51348315cf9347f431ddddf137f0245f8b3bb222
Moksh Jain <mokshjain.10a.m.com>
2024-12-27 18:40:32 +0530

    done

---
+++ expense.py
@@ -0,0 +1,19 @@
+# class Expense:
+#     # ->None implies that its not returning any value
+#     def __init__(self, name, category, amount) -> None:
+#         self.name = name
+#         self.category = category
+#         self.amount = amount
+
+#     def __repr__(self):
+#         return f"<Expense: {self.name} , {self.category}, ${self.amount:.2f} \n >"
+
+
+class Expense:
+    def __init__(self, name, category, amount) -> None:
+        self.name = name
+        self.category = category
+        self.amount = amount
+
+    def __repr__(self):
+        return f"<Expense: {self.name}, {self.category}, ${self.amount:.2f} >"
